76e6aafd7e287a9b9524f5a76f49a0bbdbb7dfee
David Henry <david_freeman_henry@hotmail.com>
2025-09-09 11:22:36 -0400

    added previous files and folders to repository

---
+++ Week1/lab1.js
@@ -0,0 +1,12 @@
+//alert("Hello, world");
+
+var userName = "David"; //this is a string
+var age = 40; //this is a number
+var daysInYear = 365; //this is a number
+var ageInDays; //this is undefined but will be a number
+var daysOld = " days old."; //this is a string
+
+ageInDays = age * daysInYear;
+//alert(ageInDays);
+
+alert(userName + " is " + ageInDays + daysOld + " More or less...");
